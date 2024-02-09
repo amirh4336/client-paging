d509c24f6c9aae84ce416d09bdc7490b9f78930e
amirh4336 <amirhossienheydarpoor@gmail.com>
2024-02-09 21:51:48 +0330

    first- commit

---
+++ src/usePaginatedFetch.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import _ from "lodash"
+
+
+interface IUsePaginatedFetchProps {
+  url: string;
+  pageSize: number;
+}
+
+const usePaginatedFetch = ({ url, pageSize }: IUsePaginatedFetchProps) => {
+const [loading, setLoading] = useState(true)
+const [data, setData] = useState<any[]>([])
+
+  const getData = async  () => {
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const paginatedData = _.chunk(data , pageSize);
+
+    setData(paginatedData)
+    setLoading(false)
+  }
+};
+
+export default usePaginatedFetch;
